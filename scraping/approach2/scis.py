80bba4426f734c6d0701ec7d96959cac8f4420fc
Nitin Sai <43436336+Nitin1901@users.noreply.github.com>
2021-11-07 18:07:07 +0530

    Update scis.py

--- scraping/approach2/scis.py
+++ scraping/approach2/scis.py
@@ -19,35 +19,25 @@ fac_interests = soup.find_all("b")
 
 for i, j, k, l in zip(fac_basic, fac_phd, fac_email, fac_interests):
     temp = i.select('b')[0].get_text().strip().split('(')
-    # name.append(temp[0])
     name = temp[0]
     if len(temp) > 1:
-        # desgn.append(temp[1][:-1])
         desgn = temp[1][:-1]
     else:
-        # desgn.append("NA")
         desgn = None
 
     temp = i.select('a')
     if len(temp) > 1:
-        # website.append(temp[1]["href"])
         website = temp[1]["href"]
     else:
-        # website.append("NA")
         website = None
-    # profile.append(f'http://scis.uohyd.ac.in/{temp[0]["href"]}')
     profile = f'http://scis.uohyd.ac.in/{temp[0]["href"]}'
 
     temp = i.select("td")[1].get_text().split(':')[1].strip()
     if temp != '':
-        # ph_num.append(temp)
         ph_num = temp
     else:
-        # ph_num.append("NA")
         ph_num = None
 
-    # phd.append(j.get_text())
-    # email.append(k.get_text().split('\\')[0].strip())
     phd = j.get_text()
     email = k.get_text().split('\\')[0].strip()
 
