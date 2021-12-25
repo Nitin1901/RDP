e41325e7afce1d890a682bf5dce2f1a6c2c4f340
Roopa Yashaswini <ryk.2001@gmail.com>
2021-12-25 15:40:34 +0530

    round 2 basic scrape

---
+++ scraping/round 2/basic.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome (executable_path="C:\\Program Files (x86)\\chromedriver.exe")
+driver.maximize_window()
+driver.get("https://irins.org/irins/a/searchc/search")
+
+faculty = []
+
+if len(driver.find_elements_by_class_name('list-product-description')) > 0:
+    rows = driver.find_elements_by_class_name('list-product-description')
+    for row in rows:
+        info = row.text.split('\n')
+        name = info[1]
+        designation = info[2]
+        university = info[-6]
+        dept = info[-7]
+        link_el = row.find_element_by_class_name('col-sm-3')
+        link = link_el.find_element_by_tag_name('a').get_attribute('href')
+        area = info[3]
+        interests = info[4].split(',')
+        row = {
+            "name": name,
+            "contact": {
+                "email": None, 
+                "ph_num": None
+            },
+            "affiliations": [{
+                "designation": designation,
+                "university": university,
+                "department": dept
+            }],
+            "education": None,
+            "profile": None,
+            "website": None,
+            "research": {
+                "area": area,
+                "interests": interests
+            },
+            "experience": None
+        }
+        faculty.append(row)
+
+print(faculty)
+driver.quit()
